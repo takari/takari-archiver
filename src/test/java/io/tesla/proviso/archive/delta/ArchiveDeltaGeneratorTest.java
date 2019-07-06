cc2fc494c038ebcfdccb8e5afa3edbde12a106e3
Jason van Zyl <jason@vanzyl.ca>
2019-07-05 20:34:03 -0400

    Use ImmutableMap.Builder to build the maps for archives.

--- src/test/java/io/tesla/proviso/archive/delta/ArchiveDeltaGeneratorTest.java
+++ src/test/java/io/tesla/proviso/archive/delta/ArchiveDeltaGeneratorTest.java
@@ -6,7 +6,7 @@ import static io.tesla.proviso.archive.delta.ArchiveDeltaGenerator.deltaOf;
 import static io.tesla.proviso.archive.delta.Hash.hashOf;
 import static org.junit.Assert.assertEquals;
 
-import com.google.common.collect.Maps;
+import com.google.common.collect.ImmutableMap;
 import io.tesla.proviso.archive.Archiver;
 import java.io.File;
 import java.util.Map;
@@ -20,28 +20,30 @@ public class ArchiveDeltaGeneratorTest {
   public TestName name = new TestName();
 
   @Test
-  public void validateArchiveDeltaGeneration() throws Exception {
+  public void validateArchiveDeltaGenerationWhereThereAreNoDifferences() throws Exception {
 
     // Create source and target archives with additions, removals and differences
 
     Archiver archiver = Archiver.builder().build();
 
     File source = getTargetArchive("delta-source-0.jar");
-    Map<String, String> sourceEntries = Maps.newLinkedHashMap();
-    sourceEntries.put("path/", null);
-    sourceEntries.put("path/0", "0");
-    sourceEntries.put("path/1", "1");
-    sourceEntries.put("path/2", "2");
-    sourceEntries.put("path/3", "3");
+    Map<String, String> sourceEntries = new ImmutableMap.Builder<String, String>()
+        .put("path/", "")
+        .put("path/0", "0")
+        .put("path/1", "1")
+        .put("path/2", "2")
+        .put("path/3", "3")
+        .build();
     archiver.archive(source, mapSource(sourceEntries));
 
     File target = getTargetArchive("delta-target-0.jar");
-    Map<String, String> targetEntries = Maps.newLinkedHashMap();
-    sourceEntries.put("path/", null);
-    targetEntries.put("path/0", "0");
-    targetEntries.put("path/1", "1");
-    targetEntries.put("path/2", "2");
-    targetEntries.put("path/3", "3");
+    Map<String, String> targetEntries = new ImmutableMap.Builder<String, String>()
+        .put("path/", "")
+        .put("path/0", "0")
+        .put("path/1", "1")
+        .put("path/2", "2")
+        .put("path/3", "3")
+        .build();
     archiver.archive(target, mapSource(targetEntries));
 
     ArchiveDelta delta = new ArchiveDeltaGenerator().deltaOf(source, target);
@@ -49,68 +51,62 @@ public class ArchiveDeltaGeneratorTest {
     assertEquals(0, delta.additions().size());
     assertEquals(0, delta.removals().size());
     assertEquals(0, delta.differences().size());
+  }
 
-    source = getTargetArchive("delta-source-1.jar");
-    sourceEntries = Maps.newLinkedHashMap();
-    sourceEntries.put("path/0", "0");
-    sourceEntries.put("path/1", "1");
-    sourceEntries.put("path/2", "2");
+  @Test
+  public void validateArchiveDeltaGenerationWhereThereAreDifferences() throws Exception {
+
+    Archiver archiver = Archiver.builder().build();
+
+    File source = getTargetArchive("delta-source-1.jar");
+    Map<String, String> sourceEntries = new ImmutableMap.Builder<String, String>()
+        .put("path/0", "0")
+        .put("path/1", "1")
+        .put("path/2", "2")
+        .build();
     archiver.archive(source, mapSource(sourceEntries));
 
-    target = getTargetArchive("delta-target-1.jar");
-    targetEntries = Maps.newLinkedHashMap();
-    targetEntries.put("path/0", "0");
-    targetEntries.put("path/1", "1difference");
-    //targetEntries.put("path/2", "2"); // removal
-    targetEntries.put("path/3", "3addition");
+    File target = getTargetArchive("delta-target-1.jar");
+    Map<String, String> targetEntries = new ImmutableMap.Builder<String, String>()
+        .put("path/0", "0")
+        .put("path/1", "1difference") // difference
+        //targetEntries.put("path/2", "2"); // removal
+        .put("path/3", "3addition")
+        .build();
     archiver.archive(target, mapSource(targetEntries));
 
-    System.out.println();
-
-    delta = new ArchiveDeltaGenerator().deltaOf(source, target);
+    ArchiveDelta delta = new ArchiveDeltaGenerator().deltaOf(source, target);
     delta.print();
     assertEquals(1, delta.additions().size());
     assertEquals(1, delta.removals().size());
     assertEquals(1, delta.differences().size());
-
-    // Create a fleshed out delta
-
-    // Apply the delta to the source to produce the target
-
-    // Compare the constructed target to the target
-
   }
 
   @Test
-  public void validateGeneratingArchives() throws Exception {
+  public void validateGeneratingArchivesFromSourceAndDelta() throws Exception {
 
     Archiver archiver = Archiver.builder().normalize(true).build();
+
     File source = getTargetArchive("generate-archive-source-0.jar");
-    Map<String, String> sourceEntries = Maps.newLinkedHashMap();
-    sourceEntries.put("path/", null);
-    sourceEntries.put("path/0", "0");
-    sourceEntries.put("path/1", "1");
-    sourceEntries.put("path/2", "2");
-    sourceEntries.put("path/3", "3");
-    sourceEntries.put("path/4", "4");
+    Map<String, String> sourceEntries = new ImmutableMap.Builder<String, String>()
+        .put("path/", "")
+        .put("path/0", "0")
+        .put("path/1", "1")
+        .put("path/2", "2")
+        .put("path/3", "3")
+        .put("path/4", "4")
+        .build();
     archiver.archive(source, mapSource(sourceEntries));
 
     File target = getTargetArchive("generate-archive-target-0.jar");
-    Map<String, String> targetEntries = Maps.newLinkedHashMap();
-    targetEntries.put("path/", null);
-    targetEntries.put("path/0", "0");
-    targetEntries.put("path/1", "1");
-    //targetEntries.put("path/2", "2");
-    targetEntries.put("path/3", "3difference");
-    targetEntries.put("path/4", "4");
-    targetEntries.put("path/5", "5");
-    targetEntries.put("path/6", "6");
-    targetEntries.put("path/7", "7");
-    targetEntries.put("path/8", "8");
-    targetEntries.put("path/9", "9");
-    targetEntries.put("path/10", "10");
-    targetEntries.put("path/11", "11");
-
+    Map<String, String> targetEntries = new ImmutableMap.Builder<String, String>()
+        .put("path/", "")
+        .put("path/0", "0")
+        .put("path/1", "1")
+        //.put("path/2","2") // removal
+        .put("path/3", "3difference") // difference
+        .put("path/4", "4") // addition
+        .build();
     archiver.archive(target, mapSource(targetEntries));
 
     File generatedTarget = getTargetArchive("generate-archive-target-1.jar");
